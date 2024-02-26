832aaeed352286ad8cfc2017a17ab77a79ad5091
Jose Manuel <joseman6575@gmail.com>
2024-02-25 19:28:15 -0500

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import { ListGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <h1 className="text-center">Todos</h1>
+      <ListGroup>
+        <ListGroup.Item className="p-0">
+          <input
+            className="container-fluid p-3 border-0"
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              console.log(e);
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="What do you need to do"
+          />
+        </ListGroup.Item>
+        {todos.map((item, index) => (
+          <ListGroup.Item className="p-3 d-flex justify-content-between">
+            <p className="m-0 d-flex align-items-center">{item}</p>
+            <button
+              type="button"
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index !== currentIndex)
+                )
+              }
+              class="btn btn-light"
+            >
+              <FontAwesomeIcon icon={faX} />
+            </button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <p>{todos.length} items left</p>
+    </div>
+  );
 };
 
 export default Home;
